df5c3306b498941e94dd8d4de10c0f7c95340f59
Lilwm <lmiiri95@gmail.com>
2023-01-12 17:40:01 +0300

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,4 +0,0 @@
-# sets up a client SSH configuration file so that we can connect to a server without using a password
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
-}

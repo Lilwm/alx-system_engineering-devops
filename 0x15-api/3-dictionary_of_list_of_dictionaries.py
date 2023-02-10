8f15839466397c9be1a1e576a63c31ed40826d06
lilwm <lmiiri95@gmail.com>
2023-02-10 17:16:54 +0000

    corrected module documentation

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,75 @@
-import requests
+#!/usr/bin/python3
+
 import json
+import requests
 
-def todo_lists_all_employees():
-    # Get information about all employees
-    employee_response = requests.get("https://jsonplaceholder.typicode.com/users")
-    employee_data = employee_response.json()
-    employee_map = {employee["id"]: employee["username"] for employee in employee_data}
-    
-    # Get TODO lists for all employees
-    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos")
-    todo_data = todo_response.json()
-    
-    # Write the TODO list data to a JSON file
-    with open("todo_all_employees.json", "w") as jsonfile:
-        tasks = {}
-        for task in todo_data:
-            user_id = task["userId"]
-            if user_id not in tasks:
-                tasks[user_id] = []
-            tasks[user_id].append({"username": employee_map[user_id], "task": task["title"], "completed": task["completed"]})
-        json.dump(tasks, jsonfile)
+'''
+This module contains functions to gather information about employee TODO list progress and export the data in JSON format.
+'''
 
-if __name__ == "__main__":
-    todo_lists_all_employees()
+def gather_data_from_an_API(employee_id):
+    """
+    This function accepts an integer (employee ID) as a parameter and returns information about employee's TODO list progress.
+
+    :param employee_id: ID of the employee
+    :return: information about employee's TODO list progress
+    """
+    # API endpoint to get all tasks
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    # Get response from API
+    response = requests.get(url)
+    # Check if the API call was successful
+    if response.status_code == 200:
+        # Convert API response to JSON
+        todos = response.json()
+        # API endpoint to get employee information
+        url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+        # Get response from API
+        response = requests.get(url)
+        # Convert API response to JSON
+        user = response.json()
+        # Get employee name
+        employee_name = user["username"]
+        # Get completed tasks
+        completed_tasks = [todo for todo in todos if todo["completed"]]
+        # Get total number of tasks
+        total_tasks = len(todos)
+        # Get number of completed tasks
+        completed_tasks_count = len(completed_tasks)
+        # Return employee TODO list progress
+        return {
+            "employee_name": employee_name,
+            "completed_tasks_count": completed_tasks_count,
+            "total_tasks": total_tasks,
+            "completed_tasks": completed_tasks
+        }
+    else:
+        # If API call was not successful, return None
+        return None
+
+def export_data_to_json(employee_id):
+    """
+    This function accepts an integer (employee ID) as a parameter and exports information about employee's TODO list progress in JSON format.
+
+    :param employee_id: ID of the employee
+    :return: None
+    """
+    # Get employee TODO list progress
+    data = gather_data_from_an_API(employee_id)
+    # Check if data was returned
+    if data:
+        # Create a list to store task data
+        task_data = []
+        # Get employee name
+        employee_name = data["employee_name"]
+        # Get completed tasks
+        completed_tasks = data["completed_tasks"]
+        # Loop through completed tasks
+        for task in completed_tasks:
+            task_data.append({"username": employee_name, "task": task["title"], "completed": task["completed"]})
+        # Create a dictionary to store all task data
+        todo_data = {employee_id: task_data}
+        # Export data to JSON file
+        with open(f"{employee_id}.json", "w") as file:
+            json.dump
 

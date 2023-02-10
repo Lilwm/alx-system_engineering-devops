2a9043c61c39d3892dcb93a14740d82ef948d432
Lilwm <lmiiri95@gmain.com>
2023-02-10 23:49:12 +0300

    pep8 changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,22 +8,21 @@ def todo_list_progress(user_id):
     # Get the employee information
     emp_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     employee_response = requests.get(emp_url)
-    employee_name = employee_response.json().get("name")
+    name = employee_response.json().get("name")
 
     # Get the TODO list for the employee
     todo_url = "https://jsonplaceholder.typicode.com/users/" user_id + "/todos"
     todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
-    
+    todo_data = todo_response.json()    
     # Count the number of completed tasks
     completed_tasks = [task for task in todo_data if task["completed"] is True]
     total_tasks = len(todo_data)
     done_tasks = len(completed_tasks)
-    
     # Display the employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    print(f"Employee {name} is done with tasks({done_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task['title']}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
